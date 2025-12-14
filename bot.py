88f909f3710cd19280523473cb1fab0ff7fe1bdd
LoicRU <loic.ruyssen@epitech.eu>
2025-12-14 19:38:17 +0100

    add new features

--- bot.py
+++ bot.py
@@ -19,4 +19,20 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("🏓 Pong !")
 
+@bot.command()
+async def say(ctx, *, message):
+    await ctx.send(message)
+
+@bot.command()
+async def userinfo(ctx, member: discord.Member = None):
+    member = member or ctx.author
+    embed = discord.Embed(title=f"User Info - {member}", color=discord.Color.blue())
+    embed.add_field(name="Username", value=str(member), inline=True)
+    embed.add_field(name="ID", value=member.id, inline=True)
+    embed.add_field(name="Bot?", value=member.bot, inline=True)
+    embed.add_field(name="Joined Server", value=member.joined_at.strftime("%Y-%m-%d"), inline=True)
+    embed.add_field(name="Created Account", value=member.created_at.strftime("%Y-%m-%d"), inline=True)
+    await ctx.send(embed=embed)
+
+
 bot.run(TOKEN)
