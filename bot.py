63b18e9fb0f05b0f8de2d63ff4a9893b49bd1b46
LoicRU <loic.ruyssen@epitech.eu>
2025-12-15 21:23:37 +0100

    ADD help command

--- bot.py
+++ bot.py
@@ -10,20 +10,22 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+bot.remove_command("help")
+
 
 @bot.event
 async def on_ready():
     print(f"✅ Bot connecté : {bot.user}")
 
-@bot.command()
+@bot.command(help="Vérifie si le bot répond")
 async def ping(ctx):
     await ctx.send("🏓 Pong !")
 
-@bot.command()
+@bot.command(help="Répète un message")
 async def say(ctx, *, message):
     await ctx.send(message)
 
-@bot.command()
+@bot.command(help="Donne toutes les info de l'utilisateur")
 async def userinfo(ctx, member: discord.Member = None):
     member = member or ctx.author
     embed = discord.Embed(title=f"User Info - {member}", color=discord.Color.blue())
@@ -34,5 +36,20 @@ async def userinfo(ctx, member: discord.Member = None):
     embed.add_field(name="Created Account", value=member.created_at.strftime("%Y-%m-%d"), inline=True)
     await ctx.send(embed=embed)
 
+@bot.command(help="liste toutes les commandes")
+async def help(ctx):
+    help = discord.Embed(
+        title="📖 Aide - Commandes",
+        color=discord.Color.green()
+    )
+
+    for command in bot.commands:
+        help.add_field(
+            name=f"!{command.name}",
+            value=command.help or "Pas de description",
+            inline=False
+        )
+
+    await ctx.send(embed=help)
 
 bot.run(TOKEN)
